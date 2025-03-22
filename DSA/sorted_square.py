32561b436ece8a3579e0dc6dea15df50780b98d1
Mithun <snaikmithun15@gmail.com>
2025-03-22 12:45:35 +0530

    22-03-2025

--- DSA/sorted_square.py
+++ DSA/sorted_square.py
@@ -22,3 +22,26 @@ class Solution(object):
 #Time and Space : O(N) & O(N)
 
 # Instead of array result we can use deque colleciton.deque (If there memory constraint then deque can be used since it grows dynamically) 
+
+
+
+from collections import deque
+
+class Solution(object):
+    def sortedSquares(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        result = deque()
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            if abs(nums[l]) > abs(nums[r]):
+                result.appendleft(nums[l] ** 2)  # Insert at front
+                l += 1
+            else:
+                result.appendleft(nums[r] ** 2)  # Insert at front
+                r -= 1
+
+        return list(result)  # Convert deque back to list
